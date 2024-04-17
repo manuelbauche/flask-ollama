c5a08271139f850b69575fe909bb8ef8681ae223
Manuel <manuelbauche@hotmail.com>
2024-04-17 18:18:51 -0400

    Authentication working

--- flask_backend/api/auth.py
+++ flask_backend/api/auth.py
@@ -2,28 +2,40 @@
 Authentication logic
 '''
 
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import create_access_token
 from models import User
 from extensions import db
+from sqlalchemy import exc
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/register', methods=['POST'])
 def register():
+    '''
+    :data: {username: str, password: str}
+    :return: Success or error message and commit new user to db.
+    '''
     data = request.get_json()
     hashed_password = generate_password_hash(data['password'], method='sha256')
     new_user = User(username=data['username'], password=hashed_password)
     db.session.add(new_user)
-    db.session.commit()
-    return jsonify({'message': 'Registered successfully'}), 200
+    try:
+        db.session.commit()
+        return jsonify({'message': 'Username registered successfully'}), 200
+    except exc.IntegrityError:
+        return jsonify({'message': 'Username already exists'}), 400 # Rollback on error default is true
 
 @auth.route('/login', methods=['POST'])
 def login():
+    '''
+    :data: {username: str, password: str}
+    :return: Success or error message and access token.
+    '''
     data = request.get_json()
     user = User.query.filter_by(username=data['username']).first()
     if not user or not check_password_hash(user.password, data['password']):
-        return make_response('Incorrect login data', 401)
+        return jsonify({'message': 'Incorrect login data'}), 401
     token = create_access_token(identity=user.username)
     return jsonify({'token': token}), 200
--- flask_backend/models.py
+++ flask_backend/models.py
@@ -5,6 +5,7 @@ Define models for API endpoints
 from extensions import db
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
